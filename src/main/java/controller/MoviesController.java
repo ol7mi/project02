d4052b6808b4b61da32a8119ba95c5a9b374face
SSH <kps10011@naver.com>
2024-06-07 15:34:24 +0900

    controller add

--- src/main/java/controller/MoviesController.java
+++ src/main/java/controller/MoviesController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import dao.MoviesDAO;
 import dto.MoviesDTO;
 
-@WebServlet("/MoviesController")
+@WebServlet("*.movies")
 public class MoviesController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
